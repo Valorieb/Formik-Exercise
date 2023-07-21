583baa52fd457fbc36dece1790eca1883840899b
Valorieb <valoriebroderick@gmail.com>
2023-07-21 16:10:11 -0400

    adds dependencies and basic form structure with submit button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from "react";
+import { useFormik } from "formik";
 // TODO: import useFormik from formik library
 
 function App() {
   // TODO: add a const called formik assigned to useFormik()
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log("form:", values);
+    },
+  });
 
   return (
     <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Name</div>
+        <input
+          name="name"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        <div>Email</div>
+        <input
+          name="email"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <div>Password</div>
+        <input
+          name="password"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
